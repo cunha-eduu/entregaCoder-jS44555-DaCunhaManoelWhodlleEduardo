e692e3e95f83a49b09a33a52ac7835c2534d46c5
Eduardo Da Cunha <w.eduardo2502@gmail.com>
2022-11-27 15:51:33 -0300

    Alert adicionado. Ajustes en el codigo JS

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 
-
-const dolarUsVenta = 172
 const dolarUsCompra = 164
+const dolarUsVenta = 172
+
+
+alert(`¡Hola y bienvenido(a) a United Exchange! \r\r Dolar oficial hoy: \r Compra: $${dolarUsCompra} \r Venta: $${dolarUsVenta}`)
 
 const compraVenta = prompt("¿Usted quiere comprar o vender dolares?")
-const montoIngresado = prompt("Ingrese el monto que usted quiere " + compraVenta) 
+const montoIngresado = prompt(`Ingrese el monto que usted quiere ${compraVenta}`) 
 
 
 function cotizardor(compraVenta, montoIngresado ) {
-    if (compraVenta.toLocaleLowerCase() == "vender" && montoIngresado  >= 0) {
-        return  "El total a recibir es: AR$" + montoIngresado * dolarUsCompra + " (Pesos argentinos)"
-    } else if (compraVenta.toLocaleLowerCase()  == "comprar" && montoIngresado >= 0) {
-        return "El total a pager es: AR$" + montoIngresado * dolarUsVenta + " (Pesos argentinos)"
+    if (compraVenta.toLocaleLowerCase() === "vender" && montoIngresado  >= 0) {
+        return  "El total a recibir es: $" + montoIngresado * dolarUsCompra + " pesos argentinos"
+    } else if (compraVenta.toLocaleLowerCase()  === "comprar" && montoIngresado >= 0) {
+        return "El total a pager es: $" + montoIngresado * dolarUsVenta + " pesos argentinos"
     } else {
         return "Error: Por favor, ingrese un valor válido"
     }
