0d3ebf73bb16d0a933ab4ce6c9c3155c547054a2
Eduardo Da Cunha <w.eduardo2502@gmail.com>
2022-12-09 14:53:57 -0300

    PreEntrega2-Commit1 - diferentes tipos de monedas

--- js/main.js
+++ js/main.js
@@ -1,23 +1,64 @@
+alert(`¡Hola y bienvenido(a) a United Exchange! \r\r Dolar oficial hoy - Compra: $164  |  Venta: $172\r\r Real oficial hoy - Compra: $28  |  Venta: $32\r\r Euro oficial hoy - Compra: $178  |  Venta: $179`)
 
-const dolarUsCompra = 164
-const dolarUsVenta = 172
+const elegirMoneda = prompt("¿Qué moneda usted quiere cambiar?")
+const compraVenta = prompt(`¿Usted quiere comprar o vender ${elegirMoneda}?`)
+const montoIngresado = prompt(`Ingrese el monto en ${elegirMoneda} que usted quiere ${compraVenta}`) 
+
+const tipoMonedaArray = [
+    {
+        tipoMoneda: "dolar",
+        dolarUsCompra: 164,
+        dolarUsVenta: 172
+    },
+    {
+        tipoMoneda: "real",
+        realBrCompra: 28,
+        realBrVenta: 32
+    },
+    {
+        tipoMoneda: "euro",
+        euroEuCompra: 178,
+        euroEuVenta: 179
+    },
+
+]
 
 
-alert(`¡Hola y bienvenido(a) a United Exchange! \r\r Dolar oficial hoy: \r Compra: $${dolarUsCompra} \r Venta: $${dolarUsVenta}`)
+for (let i = 0; i < tipoMonedaArray.length; i++) {
+    
+    function cotizador(compraVenta, montoIngresado) {
 
-const compraVenta = prompt("¿Usted quiere comprar o vender dolares?")
-const montoIngresado = prompt(`Ingrese el monto que usted quiere ${compraVenta}`) 
+        //DOLAR
+        if (compraVenta.toLocaleLowerCase() === "vender" && montoIngresado  >= 0 && elegirMoneda.toLocaleLowerCase() === "dolar") {
+            return  "El total a recibir es: $" + montoIngresado * tipoMonedaArray[i].dolarUsCompra + " pesos argentinos"
+        } else if (compraVenta.toLocaleLowerCase()  === "comprar" && montoIngresado >= 0 &&  elegirMoneda.toLocaleLowerCase() === "dolar") {
+            return "El total a pagar es: $" + montoIngresado * tipoMonedaArray[i].dolarUsVenta + " pesos argentinos"
+        }  
+        
+      //REAL        
+        else if (compraVenta.toLocaleLowerCase() === "vender" && montoIngresado  >= 0 &&  elegirMoneda.toLocaleLowerCase() === "real") {
+            return  "El total a recibir es: $" + montoIngresado * tipoMonedaArray[i].realBrCompra + " pesos argentinos"
+        } else if (compraVenta.toLocaleLowerCase()  === "comprar" && montoIngresado >= 0 &&  elegirMoneda.toLocaleLowerCase() === "real") {
+            return "El total a pagar es: $" + montoIngresado * tipoMonedaArray[i].realBrVenta + " pesos argentinos"
+        }
+        
+        //EURO        
+          else if (compraVenta.toLocaleLowerCase() === "vender" && montoIngresado  >= 0 &&  elegirMoneda.toLocaleLowerCase() === "euro") {
+            return  "El total a recibir es: $" + montoIngresado * tipoMonedaArray[i].euroEuCompra + " pesos argentinos"
+        } else if (compraVenta.toLocaleLowerCase()  === "comprar" && montoIngresado >= 0 &&  elegirMoneda.toLocaleLowerCase() === "euro") {
+            return "El total a pagar es: $" + montoIngresado * tipoMonedaArray[i].euroEuVenta + " pesos argentinos"
+        }
+        else {
+            return "Error: Por favor, ingrese un valor válido"
+        }
 
 
-function cotizardor(compraVenta, montoIngresado ) {
-    if (compraVenta.toLocaleLowerCase() === "vender" && montoIngresado  >= 0) {
-        return  "El total a recibir es: $" + montoIngresado * dolarUsCompra + " pesos argentinos"
-    } else if (compraVenta.toLocaleLowerCase()  === "comprar" && montoIngresado >= 0) {
-        return "El total a pager es: $" + montoIngresado * dolarUsVenta + " pesos argentinos"
-    } else {
-        return "Error: Por favor, ingrese un valor válido"
     }
+
+    if (elegirMoneda.toLocaleLowerCase() === tipoMonedaArray[i].tipoMoneda) {
+        alert(cotizador(compraVenta, montoIngresado))
+    } 
+    
 }
 
-console.log(cotizardor(compraVenta, montoIngresado))
 
